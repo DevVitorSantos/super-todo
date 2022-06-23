37a641610b7dbd75f61c433c2c96fcc74e439b36
Vitor Santos <vitorsantos.seo@gmail.com>
2022-06-23 18:53:18 -0300

    inicial setup - sass and basic config

---
+++ src/pages/index/App.js
@@ -0,0 +1,26 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="title-box">
+          <h1>Welcome to SUPER TODO</h1>
+        </div>
+        <div className="content-box">
+          
+            <ul>
+              <li>item 01</li>
+              <li>item 02</li>
+              <li>item 03</li>
+              <li>item 04</li>
+              <li>item 05</li>
+            </ul>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
